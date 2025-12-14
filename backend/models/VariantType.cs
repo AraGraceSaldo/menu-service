74f1fb3ae93a200a2742cae61755eac96d668fbe
CLienT <CLienT@DESKTOP-6NMMH13>
2025-12-15 00:33:38 +0900

    updated varianttype

--- backend/models/VariantType.cs
+++ backend/models/VariantType.cs
@@ -2,6 +2,20 @@
 
 namespace backend.Models
 {
+    public static class VariantNames
+    {
+        public const string M = "M";
+        public const string L = "L";
+        public const string SOLO = "SOLO";
+        public const string PCS3 = "3PCS";
+        public const string PCS6 = "6PCS";
+        public const string REGULAR = "REGULAR";
+
+        public static readonly IReadOnlyList<string> All = new[] { M, L, SOLO, PCS3, PCS6, REGULAR };
+
+        public static bool IsValid(string value) => !string.IsNullOrEmpty(value) && All.Contains(value);
+    }
+
     public class VariantType
     {
         [Key]
@@ -9,10 +23,9 @@ namespace backend.Models
 
         [Required]
         [MaxLength(50)]
-        public string Name { get; set; } = string.Empty;
+        public string Name { get; set; } = VariantNames.M;
 
-        // Navigation property
-        public ICollection<ItemVariant_Price> ItemVariantPrices { get; set; } = new List<ItemVariant_Price>();
+        public ICollection<ItemVariant_Price> ItemVariantPrices { get; set; }
     }
 }
 
